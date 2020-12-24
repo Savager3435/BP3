dbeabffc7694666f4facbc2224529a58664b59ca
Dev35 <38446179+Savager3435@users.noreply.github.com>
2020-12-23 22:40:22 -0700

    Add files via upload

---
+++ BP3.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+crypto = input("Enter the crypto you want the price of: ")
+
+
+URL = ('https://www.coindesk.com/price/' + crypto)
+r = requests.get(URL)
+
+soup = BeautifulSoup(r.content, 'html.parser')
+
+price = soup.find('div', attrs = {'class':'price-large'})
+
+returns = soup.find('div', attrs={'class':'percent-change-medium'})
+
+print(crypto,":")
+print(price.text)
+print(returns.text)
