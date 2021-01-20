1b3404ea19b62e5096353e4aac1d710fdd007c21
Vectis-45 <76608635+Vectis-45@users.noreply.github.com>
2021-01-20 00:24:39 -0700

    Got embed as a function
    The whole embed thing is a function now which saves a lot of space and time because rather than having a bunch of repeated lines we can just have one baseline embed and then a couple variables. It took too long to figure out how to pass the channel id through to the function

--- BP3_DiscordBot_Public.py
+++ BP3_DiscordBot_Public.py
@@ -1,7 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 import discord
-from discord.ext import commands
 
 client = discord.Client()
 
@@ -11,7 +10,7 @@ async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
 
-def price():
+def price(crypto):
     url = ('https://www.coindesk.com/price/' + crypto)
     r = requests.get(url)
 
@@ -30,39 +29,63 @@ def price():
     global rnet
     rnet = net.text
 
-    global message
-    message = price.text
-
-
-
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    def embedthing():
-        embed = discord.Embed(title='BitcoinInfo')
-        embed.set_thumbnail(
-            url='https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Bitcoin.svg/1200px-Bitcoin.svg.png')
+    global crypto
+
+    if message.content.startswith('$supported'):
+        await message.channel.send('Currently supported are: Bitcoin, Ethereum, XRP, Stellar and Chainlink')
+
+    async def dembed(name, image, mloc):
+
+        embed = discord.Embed(title=name)
+        embed.set_thumbnail(url=image)
         embed.add_field(name='Price:', value=rprice, inline=True)
         embed.add_field(name='24H Return:', value=rreturn, inline=False)
         embed.add_field(name='24H Net:', value=rnet, inline=True)
-
-    if message.content.startswith('$btc'):
-        global crypto
-        crypto = 'bitcoin'
-        price()
-
-        embed = discord.Embed(title='BitcoinInfo')
-        embed.set_thumbnail(
-            url='https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Bitcoin.svg/1200px-Bitcoin.svg.png')
-        embed.add_field(name='Price:', value=rprice, inline=True)
-        embed.add_field(name='24H Return:', value=rreturn, inline=False)
-        embed.add_field(name='24H Net:', value=rnet, inline=True)
+        channel = mloc
         await message.channel.send(embed=embed)
 
-        print(rprice)
+    if message.content.startswith('$btc'):
+        price('bitcoin')
+
+        await dembed('Bitcoin','https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Bitcoin.svg/1200px-Bitcoin.svg.png', message.id)
+
+        print('Bitcoin:', rprice)
+
+    if message.content.startswith('$eth'):
+        price('ethereum')
+
+        await dembed('Ethereum', 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Ethereum-icon-purple.svg', message.id)
+
+        print('Ethereum:', rprice)
+
+    if message.content.startswith('$xrp'):
+        price('xrp')
+
+        await dembed('XRP', 'https://upload.wikimedia.org/wikipedia/commons/2/29/Xrp-symbol-black-128.png', message.id)
+
+        print('XRP:', rprice)
+
+    if message.content.startswith('$stellar'):
+        price('stellar')
+
+        await dembed('Stellar', 'https://upload.wikimedia.org/wikipedia/commons/5/56/Stellar_Symbol.png', message.id)
+
+        print('Stellar:', rprice)
+
+    if message.content.startswith('$chainlink'):
+        price('chainlink')
+
+        await dembed('Chainlink', 'https://d141qf1r21kfi0.cloudfront.net/static/img/coins/200x200/1975.png', message.id)
+
+        print('Chainlink:', rprice)
 
 
-client.run('Your token here')
+
+
+client.run('YOUR TOKEN HERE')
